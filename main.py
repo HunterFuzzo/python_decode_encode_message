6d3ae640a29efe30fc7346d78df57542aeb0b562
Azuka <100121407+HunterFuzzo@users.noreply.github.com>
2023-12-08 09:40:21 +0100

    + class

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
-def main():
-    print("coucou")
-    print("bonjour")
+class TransmissionSequence:
+    def __init__(self, message):
+        self.message = message
+    
+    def getMessage(self):
+        return f"Voici le message : {self.message}"
 
-main()
+enteredMessage = input(str("Entrez un message a décodé : "))
+
+messageCode = TransmissionSequence(message = enteredMessage)
+
+print(messageCode.getMessage())
+
+
+# Chaine de transmission
+# Méthode de conversion ASCII
+# CAN/CNA
+# CRC : 
+# Codage CRC
+# Décodage CRC
+# Manchester :
+# Codage Manchester
+# Décodage Manchester
+# ASK :
+# Modulation ASK
+# Démodulation ASK
